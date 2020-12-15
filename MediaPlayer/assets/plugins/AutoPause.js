87cfced47ef2454f46158d6d236938734533837b
martino2197 <martino2197@gmail.com>
2020-12-14 20:57:38 -0600

    Visibility Change: forma parte del API del DOM llamado Page Visibility y nos deja saber si el elemento es visible, puede ser usado para ejecutar una acción cuando cambiamos de pestaña. Asi podemos ahorrar baterïa y mejorar la UX.

--- MediaPlayer/assets/plugins/AutoPause.js
+++ MediaPlayer/assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
   run(player) {
     this.player = player;
@@ -9,6 +10,8 @@ class AutoPause {
       threshold: this.threshold,
     });
     observer.observe(this.player.media);
+
+    document.addEventListener("visibilitychange", this.handleVisibilityChange);
   }
   handleIntersection(entries) {
     const entry = entries[0];
@@ -21,6 +24,14 @@ class AutoPause {
       this.player.pause();
     }
     console.log(entry);
+  }
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === "visible";
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
   }
 }
 
